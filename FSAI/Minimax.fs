f78f29b0228b42bdcd44fa596c7552ac7fec27aa
Filip Lundh <lufi18vo@student.ju.se>
2021-09-19 19:45:41 +0200

    commented evaluation function

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -13,23 +13,26 @@ module Minimax =
     // F# Evaluation
     let evaluate (board: byte[,], getValidMoves, getScore, countCorners) : int =
    
-            let evaluation: int = 0
-   
+            // gets the current valid moves for Black and White
             let validMovesBlack: ResizeArray<Tuple<int, int>> = getValidMoves board Black
             let validMovesWhite: ResizeArray<Tuple<int, int>> = getValidMoves board White
-   
             let blackMobility: int = validMovesBlack.Count;
             let whiteMobility: int = validMovesWhite.Count;
+
+            // gets the current score of Black and White 
             let blackScore: int = getScore board Black;
             let whiteScore: int = getScore board White;
-   
-            printf "Black score: %i White score: %i" blackScore whiteScore
-   
+      
+            // If leaf node leads to a black score of 0, chance of winning terrible
             if blackScore = 0 then
                 -200000
+            // else high chance of winning
             else if whiteScore = 0 then
                 200000
+
+            // If game is over ( max score = 64 or no more moves can be made )
             else if blackScore + whiteScore = 64 || blackMobility + whiteMobility = 0 then
+                
                 if blackScore < whiteScore then
                     -100000 - whiteScore + blackScore
                 else if blackScore > whiteScore then
@@ -37,16 +40,18 @@ module Minimax =
                 else 
                     0
             else
-                let evaluation2 = evaluation + blackScore - whiteScore
+            // returns evaluation depending on the score
+                let evaluation =  blackScore - whiteScore
    
                 if blackScore + whiteScore > 55 then
                     blackScore - whiteScore
-   
+    
                 else
-                    let evaluation3 = evaluation2 + (blackMobility - whiteMobility) * 10
-                    let evaluation4 = evaluation3 + ((countCorners board Black) - (countCorners board White)) * 100
+                    // if black has fewer moves than white -> negative evaluation, worse evaluation
+                    let evaluation2 = evaluation + (blackMobility - whiteMobility) * 10
+                    let evaluation3 = evaluation2 + ((countCorners board Black) - (countCorners board White)) * 100
    
-                    evaluation4
+                    evaluation3
            
     // F# MinimaxAlphaBeta
     let rec minimaxAlphaBeta (boardF: byte[,], depthF: int, minValueF: int, maxValueF: int, tileF: byte, isMaxPlayerF: bool, getValidMoves, makeMove, getWinner, otherTile, getScore, countCorners) =
